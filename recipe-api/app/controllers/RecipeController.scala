956fde79effdb7e6d888b03ffb397f381c5f901d
silverstar <james.harrison981208@gmail.com>
2025-05-09 02:22:06 +0800

    Initial commit for the project

---
+++ recipe-api/app/controllers/RecipeController.scala
@@ -0,0 +1,61 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import play.api.libs.json._
+import models.Recipe
+import scala.collection.mutable
+import java.time.LocalDateTime
+import java.util.concurrent.atomic.AtomicLong
+
+@Singleton
+class RecipeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  private val recipes = mutable.Map[Long, Recipe]()
+  private val idCounter = new AtomicLong(1)
+
+  def createRecipe: Action[JsValue] = Action(parse.json) { request =>
+    request.body.validate[Recipe].fold(
+      errors => BadRequest(Json.obj("message" -> "Recipe creation failed!", "required" -> "title, making_time, serves, ingredients, cost")),
+      data => {
+        val id = idCounter.getAndIncrement()
+        val recipeWithId = data.copy(id = Some(id))
+        recipes += (id -> recipeWithId)
+        Ok(Json.obj("message" -> "Recipe successfully created!", "recipe" -> Seq(recipeWithId)))
+      }
+    )
+  }
+
+  def getRecipes: Action[AnyContent] = Action {
+    Ok(Json.obj("recipes" -> recipes.values.toSeq))
+  }
+
+  def getRecipeById(id: Long): Action[AnyContent] = Action {
+    recipes.get(id) match {
+      case Some(recipe) => Ok(Json.obj("message" -> "Recipe details by id", "recipe" -> Seq(recipe)))
+      case None => NotFound(Json.obj("message" -> "No recipe found"))
+    }
+  }
+
+  def updateRecipe(id: Long): Action[JsValue] = Action(parse.json) { request =>
+    request.body.validate[Recipe].fold(
+      _ => BadRequest(Json.obj("message" -> "Recipe update failed!")),
+      data => {
+        recipes.get(id) match {
+          case Some(_) =>
+            val updatedRecipe = data.copy(id = Some(id))
+            recipes.update(id, updatedRecipe)
+            Ok(Json.obj("message" -> "Recipe successfully updated!", "recipe" -> Seq(updatedRecipe)))
+          case None => NotFound(Json.obj("message" -> "No recipe found"))
+        }
+      }
+    )
+  }
+
+  def deleteRecipe(id: Long): Action[AnyContent] = Action {
+    if (recipes.remove(id).isDefined)
+      Ok(Json.obj("message" -> "Recipe successfully removed!"))
+    else
+      NotFound(Json.obj("message" -> "No recipe found"))
+  }
+}
---
+++ recipe-api/app/models/Recipe.scala
@@ -0,0 +1,16 @@
+package models
+
+import play.api.libs.json._
+
+case class Recipe(
+  id: Option[Long],
+  title: String,
+  making_time: String,
+  serves: String,
+  ingredients: String,
+  cost: String
+)
+
+object Recipe {
+  implicit val recipeFormat: OFormat[Recipe] = Json.format[Recipe]
+}
